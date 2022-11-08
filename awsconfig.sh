ec9e0ec57166092eeddc5d4776459a3336f93557
koushik p r <karanboltz@gmail.com>
2022-11-08 23:09:13 +0530

    Update awsconfig.sh

--- awsconfig.sh
+++ awsconfig.sh
@@ -6,11 +6,11 @@ echo "==================================================="
 echo "RUN below command manually"
 echo "==================================================="
 echo python3 -m venv ansible
-echo source ~/aws-ansible/ansible/bin/activate
+echo source ~/Patient-details-using-REST-API-/ansible/bin/activate
 echo pip install pip --upgrade
 echo pip install boto
 echo pip install boto3
 echo pip install ansible
 echo aws configure 
-echo ansible-playbook  -i inventory ec2.yml
+echo ansible-playbook  -i inventory autorunning.yaml
 echo "==================================================="
